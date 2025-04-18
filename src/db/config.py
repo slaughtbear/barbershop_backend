430f527556c443addcabedcae07265ad6c29f639
slaughtbear <ivanaguirre368@gmail.com>
2025-04-17 21:12:37 -0600

    Configuración global y conexión con Supabase

---
+++ src/db/config.py
@@ -0,0 +1,24 @@
+import os
+from dotenv import load_dotenv
+from supabase import create_client, Client
+
+
+load_dotenv()
+
+
+class Config:
+    """
+    Clase de configuración global que sirve para almacenar variables de entorno como atributos y acceder a ellas mediante un objeto.
+    """
+    SUPABASE_URL: str = os.getenv("SUPABASE_URL")
+    SUPABASE_KEY: str = os.getenv("SUPABASE_KEY")
+
+
+config = Config()
+
+
+if not all ([config.SUPABASE_URL, config.SUPABASE_KEY]):
+    raise EnvironmentError("No se ha detectado una o más variables de entorno...")
+
+
+supabase: Client = create_client(config.SUPABASE_URL, config.SUPABASE_KEY)
