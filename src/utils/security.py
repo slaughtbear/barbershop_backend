72510cabd0af3ccd8f56af3caeb2939ac14650a5
slaughtbear <ivanaguirre368@gmail.com>
2025-04-19 09:24:52 -0600

    Se modifica la función encode_token para definir expiración de tiempo de los tokens

--- src/utils/security.py
+++ src/utils/security.py
@@ -1,13 +1,15 @@
 from typing import Annotated
+from datetime import datetime, timedelta, timezone
 
 import jwt
+from jwt import ExpiredSignatureError, InvalidTokenError
+
 from passlib.context import CryptContext
 from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer
 
 from src.db.config import config
 from src.db.users import users_db
-# from src.schemas.users import User
 
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl = "/login")
@@ -15,14 +17,16 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 def encode_token(payload: dict) -> str:
-    """ Genera un token a partir de la información del usuario.
+    """ Genera un token a partir de la información del usuario con tiempo de expiración.
     
     Argumentos:
         payload (Dict): Diccionario con los datos del usuario.
+        expires_in (int): Tiempo en minutos antes de que el token expire.
 
     Retorna:
         str: El token generado para el usuario.
     """
+    payload["exp"] = datetime.now(timezone.utc) + timedelta(minutes=config.TOKEN_EXPIRES_IN_MINUTES)
     token: str = jwt.encode(payload, config.SECRET_KEY, config.ALGORITHM)
     return token
 
@@ -36,8 +40,13 @@ def decode_token(token: Annotated[str, Depends(oauth2_scheme)]) -> dict:
     Retorna:
         Dict: Diccionario con la información del usuario.
     """
-    data: dict = jwt.decode(token, config.SECRET_KEY, algorithms = [config.ALGORITHM])
-    user: dict = users_db.search_by_username(data["username"])
+    try:
+        data: dict = jwt.decode(token, config.SECRET_KEY, algorithms = [config.ALGORITHM])
+        user: dict = users_db.search_by_username(data["username"])
+    except ExpiredSignatureError:
+        raise ValueError("El token ha expirado.")
+    except InvalidTokenError:
+        raise ValueError("El token es inválido.")
     return user
 
 
@@ -65,3 +74,6 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
         bool: True si coinciden, False si no coinciden.
     """
     return pwd_context.verify(plain_password, hashed_password)
+
+
+get_current_user = Annotated[dict, Depends(decode_token)] # Dependencia para obtener los datos del usuario actual
