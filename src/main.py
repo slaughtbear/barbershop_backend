4611975cf039b6b58fa02ec8ffc673fcbdbc3b81
slaughtbear <ivanaguirre368@gmail.com>
2025-04-18 20:40:22 -0600

    Se agrega router para los endpoints del sistema de autenticación

--- src/main.py
+++ src/main.py
@@ -1,4 +1,12 @@
 from fastapi import FastAPI
+from src.routes.auth import auth_router
 
 
 app = FastAPI()
+
+
+app.include_router(
+    auth_router,
+    prefix="/auth",
+    tags=["Autenticación"]
+)
