8f922a4dfd716f101aae27fbe0e0f1c83bf09977
slaughtbear <ivanaguirre368@gmail.com>
2025-04-19 09:23:05 -0600

    Se implementa tiempo de expiración para tokens

--- src/db/config.py
+++ src/db/config.py
@@ -14,6 +14,7 @@ class Config:
     SUPABASE_KEY: str = os.getenv("SUPABASE_KEY")
     SECRET_KEY: str = os.getenv("SECRET_KEY")
     ALGORITHM: str = os.getenv("ALGORITHM")
+    TOKEN_EXPIRES_IN_MINUTES: int = 60
 
 
 config = Config()
